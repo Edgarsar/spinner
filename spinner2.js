673a590d72ff216e78c1f62c26288271e33dfe7d
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-19 03:41:41 +0000

    add spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,49 +1,13 @@
-// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let char = "| / - \\ | / - \\ |";
 
-let delay = 100
-let char = "| / - \\"
-char += "\n"
+let delay = 100;
+
 for( let i = 0;i<char.length;i++){
 setTimeout(() => {
   process.stdout.write(`\r${char[i]}   `);
 
 }, delay+=200);
 }
+setTimeout(() => {   console.log(''); }, delay); 
+ 
 
-/* setTimeout(() => {
-  process.stdout.write("\r/   ");
-}, 300);
-
-
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 700);
-
-
-
-setTimeout(() => {
-  process.stdout.write("\r|   ");
-}, 1000);
-
-
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1200);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1400);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1600);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1900); */
